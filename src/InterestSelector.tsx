a412a60aef49949154d5eae492b1761a3be3a530
CryptoChaos_dev <thelastwhalegame@proton.me>
2025-07-18 15:56:48 +0300

    Import scrollbar styles in InterestSelector component to enhance UI appearance.

--- src/InterestSelector.tsx
+++ src/InterestSelector.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { INTEREST_CATEGORIES } from "./categories";
+import "./scrollbar.css";
 
 interface InterestSelectorProps {
   selected: string[];
